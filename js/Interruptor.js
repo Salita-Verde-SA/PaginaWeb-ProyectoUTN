c394cf51f2cd021df5913558d6b3adf8648211e1
Blindd <jmluna200420@gmail.com>
2025-09-19 14:20:08 -0300

    Add Comentarios y pequeños arreglos

--- js/Interruptor.js
+++ js/Interruptor.js
@@ -4,14 +4,14 @@ const moon = document.querySelector('.bx-moon');
 
 toggle.addEventListener('change', () => {
   if (toggle.checked) {
-    // 🌞 Activa modo claro → gira el sol
+    // Activa modo claro → gira el sol
     sun.classList.add('girar');
     moon.classList.remove('mecer');
 
     setTimeout(() => sun.classList.remove('girar'), 1000);
 
   } else {
-    // 🌙 Activa modo oscuro → mece la luna
+    // Activa modo oscuro → mece la luna
     moon.classList.add('mecer');
     sun.classList.remove('girar');
 
