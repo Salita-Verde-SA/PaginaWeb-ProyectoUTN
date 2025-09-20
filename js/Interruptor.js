a4a6c857fe5e5b5779d48de8f1c262b75593246c
Manuel Palmero <manueligna06@gmail.com>
2025-09-19 21:05:14 -0300

    Fix

--- js/Interruptor.js
+++ js/Interruptor.js
@@ -1,20 +1,22 @@
-const toggle = document.querySelector('.interruptor input');
-const sun = document.querySelector('.bx-sun');
-const moon = document.querySelector('.bx-moon');
+const toggle = document.querySelector(".interruptor input");
+const sun = document.querySelector(".bx-sun");
+const moon = document.querySelector(".bx-moon");
+const boton = document.querySelector(".interruptor input");
+const body = document.body;
 
-toggle.addEventListener('change', () => {
+toggle.addEventListener("change", () => {
+  body.classList.toggle("claro");
   if (toggle.checked) {
     // Activa modo claro → gira el sol
-    sun.classList.add('girar');
-    moon.classList.remove('mecer');
+    sun.classList.add("girar");
+    moon.classList.remove("mecer");
 
-    setTimeout(() => sun.classList.remove('girar'), 1000);
-
+    setTimeout(() => sun.classList.remove("girar"), 1000);
   } else {
     // Activa modo oscuro → mece la luna
-    moon.classList.add('mecer');
-    sun.classList.remove('girar');
+    moon.classList.add("mecer");
+    sun.classList.remove("girar");
 
-    setTimeout(() => moon.classList.remove('mecer'), 1000);
+    setTimeout(() => moon.classList.remove("mecer"), 1000);
   }
 });
