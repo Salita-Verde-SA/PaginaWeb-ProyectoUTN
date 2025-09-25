69d7d21a63aa0949001e061bde0db9419c4acd5e
Blindd <jmluna200420@gmail.com>
2025-09-25 09:57:56 -0300

    Añadido soporte para transiciones suaves en la barra lateral y contenido principal; optimizado el manejo de clases 'claro' en el script de interruptor.

--- js/Interruptor.js
+++ js/Interruptor.js
@@ -15,6 +15,9 @@ document.addEventListener("headerContentLoaded", function () {
   const header = document.querySelector("header");
   const footer = document.querySelector("footer");
   const localidad = document.querySelector(".barra-navegacion-izquierda");
+  const contendorGeneros = document.querySelector(".barra-lateral-izquierda");
+  const contenedorEventos = document.querySelector(".barra-lateral-derecha");
+  const contenedorViewAll = document.querySelector(".contenido-principal");
   const barrabusqueda = document.querySelector(".barra-busqueda");
 
   // Verifica que todos los elementos necesarios existan antes de continuar
@@ -29,6 +32,9 @@ document.addEventListener("headerContentLoaded", function () {
     body.classList.toggle("claro");
     header.classList.toggle("claro");
     footer.classList.toggle("claro");
+    contendorGeneros.classList.toggle("claro");
+    contenedorEventos.classList.toggle("claro");
+    contenedorViewAll.classList.toggle("claro");
 
     if (barrabusqueda != null) {
       barrabusqueda.classList.toggle("claro");
@@ -55,42 +61,45 @@ document.addEventListener("headerContentLoaded", function () {
     }, 125);
   });
 });
-document.querySelector('.menos').addEventListener('click', () => {
-  let valor = parseInt(document.querySelector('.valor').textContent);
-  if (valor > 0) {
-    document.querySelector('.valor').textContent = valor - 1;
-  }
-});
 
-document.querySelector('.mas').addEventListener('click', () => {
-  let valor = parseInt(document.querySelector('.valor').textContent);
-  document.querySelector('.valor').textContent = valor + 1;
-});
-/**
- * Función para alternar la visibilidad del menú de usuario.
- * Si el menú está oculto, lo muestra, y viceversa.
- */
-function toggleMenu() {
-  // Selecciona el elemento del menú de usuario por su ID
-  const userMenu = document.getElementById("userMenu");
-  // Alterna la clase 'active' en el menú de usuario.
-  // Si la clase está presente, la quita (ocultando el menú).
-  // Si no está presente, la añade (mostrando el menú).
-  userMenu.classList.toggle("active");
-}
+// TODO : Eliminar lo siguiebte si no se usa o si se implementa de otra forma más adelante, agregar condicional para que no falle en páginas sin estos elementos
 
-// Escucha los clics en cualquier parte del documento
-document.addEventListener("click", function(event) {
-  // Selecciona el elemento del menú de usuario por su ID
-  const userMenu = document.getElementById("userMenu");
-  // Selecciona el elemento del encabezado del perfil por su clase
-  const perfilHeader = document.querySelector(".perfil-header");
+// document.querySelector('.menos').addEventListener('click', () => {
+//   let valor = parseInt(document.querySelector('.valor').textContent);
+//   if (valor > 0) {
+//     document.querySelector('.valor').textContent = valor - 1;
+//   }
+// });
 
-  // Verifica si el clic ocurrió fuera del área del perfil
-  if (!perfilHeader.contains(event.target)) {
-    // Si el clic fue fuera del área del perfil, remueve la clase 'active'
-    // del menú de usuario, ocultándolo
-    userMenu.classList.remove("active");
-  }
-});
+// document.querySelector('.mas').addEventListener('click', () => {
+//   let valor = parseInt(document.querySelector('.valor').textContent);
+//   document.querySelector('.valor').textContent = valor + 1;
+// });
+// /**
+//  * Función para alternar la visibilidad del menú de usuario.
+//  * Si el menú está oculto, lo muestra, y viceversa.
+//  */
+// function toggleMenu() {
+//   // Selecciona el elemento del menú de usuario por su ID
+//   const userMenu = document.getElementById("userMenu");
+//   // Alterna la clase 'active' en el menú de usuario.
+//   // Si la clase está presente, la quita (ocultando el menú).
+//   // Si no está presente, la añade (mostrando el menú).
+//   userMenu.classList.toggle("active");
+// }
+
+// // Escucha los clics en cualquier parte del documento
+// document.addEventListener("click", function(event) {
+//   // Selecciona el elemento del menú de usuario por su ID
+//   const userMenu = document.getElementById("userMenu");
+//   // Selecciona el elemento del encabezado del perfil por su clase
+//   const perfilHeader = document.querySelector(".perfil-header");
+
+//   // Verifica si el clic ocurrió fuera del área del perfil
+//   if (!perfilHeader.contains(event.target)) {
+//     // Si el clic fue fuera del área del perfil, remueve la clase 'active'
+//     // del menú de usuario, ocultándolo
+//     userMenu.classList.remove("active");
+//   }
+// });
 
