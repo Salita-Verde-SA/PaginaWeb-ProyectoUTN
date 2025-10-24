1071fc4fddc187a3d2a29e2963b63d6ea34b2ac7
Manuel Palmero <manueligna06@gmail.com>
2025-10-24 09:43:50 -0300

    Eliminar importación innecesaria de List en UsuarioRepository y actualizar subproyecto frontend a la última versión.

--- backend/src/main/java/com/salitaverde/backend/backend/repository/UsuarioRepository.java
+++ backend/src/main/java/com/salitaverde/backend/backend/repository/UsuarioRepository.java
@@ -4,7 +4,6 @@ import com.salitaverde.backend.backend.model.mongo.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
